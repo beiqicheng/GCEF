21979f6be49c89eae2505f2f5a9bd889f21008e1
Qicheng <87012722+beiqicheng@users.noreply.github.com>
2023-05-17 09:57:32 +0200

    Update blastn.sh

--- GCEF-Model/blastn.sh
+++ GCEF-Model/blastn.sh
@@ -10,6 +10,8 @@
 module load foss/2019b BLAST+/2.9.0
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK:-1}
 
-file=$(ls *.fasta | sed -n ${SLURM_ARRAY_TASK_ID}p)
+file=$(ls *.fa | sed -n ${SLURM_ARRAY_TASK_ID}p)
+
+### Build blastn database using: makeblastdb -in Silva-V34s.fasta -dbtype nucl -out Silva-V34s
 
 blastn -query $file -out $file.out -db Silva-V34s -evalue 1e-5 -num_threads ${SLURM_CPUS_PER_TASK:-1} -perc_identity 90 -max_target_seqs 1 -outfmt "6 qseqid sseqid pident length sseq"
