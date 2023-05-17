d1f50aca0f055147ad4feeee7be1c37c6433af13
Qicheng <87012722+beiqicheng@users.noreply.github.com>
2023-05-17 09:53:38 +0200

    Update blastn.sh

--- GCEF-Model/blastn.sh
+++ GCEF-Model/blastn.sh
@@ -2,16 +2,14 @@
 #SBATCH --chdir=/work/bei/GCEF/MEGAHIT/contigs/
 #SBATCH --output=/work/bei/GCEF/MEGAHIT/contigs/%x-%A-%a.out
 #SBATCH --error=/work/bei/GCEF/MEGAHIT/contigs/%x-%A-%a.err
-#SBATCH --time=24:00:00
-#SBATCH --mem-per-cpu=4G
-#SBATCH --cpus-per-task=10
+#SBATCH --time=01:00:00
+#SBATCH --mem-per-cpu=2G
+#SBATCH --cpus-per-task=4
 #SBATCH -J blastn
-#SBATCH --mail-user=qicheng.bei@ufz.de
-#SBATCH --mail-type=BEGIN,END,FAIL
 
 module load foss/2019b BLAST+/2.9.0
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK:-1}
 
-file=$(ls *.fa | sed -n ${SLURM_ARRAY_TASK_ID}p)
+file=$(ls *.fasta | sed -n ${SLURM_ARRAY_TASK_ID}p)
 
-blastn -query $file -out $file.out -db V34-big -evalue 1e-5 -num_threads ${SLURM_CPUS_PER_TASK:-1} -perc_identity 90 -max_target_seqs 1 -outfmt "6 qseqid sseqid pident length sseq"
+blastn -query $file -out $file.out -db Silva-V34s -evalue 1e-5 -num_threads ${SLURM_CPUS_PER_TASK:-1} -perc_identity 90 -max_target_seqs 1 -outfmt "6 qseqid sseqid pident length sseq"
