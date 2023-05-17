6123c91c2b2c812aa913726940d80cd566e83237
Qicheng <87012722+beiqicheng@users.noreply.github.com>
2023-05-17 09:59:56 +0200

    Update blastn.sh

--- GCEF-Model/blastn.sh
+++ GCEF-Model/blastn.sh
@@ -14,4 +14,5 @@ file=$(ls *.fa | sed -n ${SLURM_ARRAY_TASK_ID}p)
 
 ### Build blastn database using: makeblastdb -in Silva-V34s.fasta -dbtype nucl -out Silva-V34s
 
-blastn -query $file -out $file.out -db Silva-V34s -evalue 1e-5 -num_threads ${SLURM_CPUS_PER_TASK:-1} -perc_identity 90 -max_target_seqs 1 -outfmt "6 qseqid sseqid pident length sseq"
+blastn -query $file -out $file.out -db Silva-V34s -evalue 1e-5 -num_threads ${SLURM_CPUS_PER_TASK:-1} -perc_identity 90 -max_target_seqs 1 -outfmt "6 qseqid sseqid pident length sseq" 
+### The sequence identity and output format could be changed according to the purpose of study.
